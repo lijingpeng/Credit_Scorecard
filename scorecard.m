f0285d1b6f4827e36bf0a497cc1a80bcc1ddfd6b
shucai.ljp <shucai.ljp@alibaba-inc.com>
2016-06-02 20:46:11 +0800

    add comment

--- scorecard.m
+++ scorecard.m
@@ -3,40 +3,44 @@ sc = creditscorecard(data,'IDVar','CustID');
 bininfo(sc,'ResStatus')
 plotbins(sc,'ResStatus')
 
+% auto binning
 cp = 20000:5000:60000;
 sc = modifybins(sc,'CustIncome','CutPoints',cp);
 bininfo(sc,'CustIncome')
 plotbins(sc,'CustIncome')
+
 sc = autobinning(sc);
 [bi,cp] = bininfo(sc,'CustAge');
 cp([1 5]) = []; % To merge bins 1 and 2, and bins 5 and 6
 sc = modifybins(sc,'CustAge','CutPoints',cp);
 plotbins(sc,'CustAge')
+
 [bi,cp] = bininfo(sc,'CustIncome');
 cp([3 4]) = [];
 sc = modifybins(sc,'CustIncome','CutPoints',cp);
 plotbins(sc,'CustIncome')
+
 [bi,cp] = bininfo(sc,'TmWBank');
 cp(2) = [];
 sc = modifybins(sc,'TmWBank','CutPoints',cp);
 plotbins(sc,'TmWBank')
+
 [bi,cp] = bininfo(sc,'AMBalance');
 cp(2) = [];
 sc = modifybins(sc,'AMBalance','CutPoints',cp);
 plotbins(sc,'AMBalance')
 
+% Fit model
 sc = fitmodel(sc);
+
 p1 = displaypoints(sc);
 disp(p1)
 sc = modifybins(sc,'CustAge','BinLabels',...
 {'Up to 36' '37 to 39' '40 to 45' '46 to 57' '58 and up'});
-
 sc = modifybins(sc,'CustIncome','BinLabels',...
 {'Up to 28999' '29000 to 32999' '33000 to 41999' '42000 to 46999' '47000 and up'});
-
 sc = modifybins(sc,'TmWBank','BinLabels',...
 {'Up to 11' '12 to 44' '45 to 70' '71 and up'});
-
 sc = modifybins(sc,'AMBalance','BinLabels',...
 {'Up to 558.87' '558.88 to 1597.43' '1597.44 and up'});
 
